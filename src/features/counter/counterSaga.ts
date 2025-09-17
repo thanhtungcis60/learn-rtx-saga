d8e84f6e62340e58fc0a33a9960e4e4f614b5198
ttt <thanhtungcis60@gmail.com>
2025-09-17 11:58:18 +0700

    Redux Saga: 06 - Phân biệt takeEvery và takeLatest

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,13 +1,20 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { take, takeEvery } from 'redux-saga/effects';
-import { increment } from './counterSlice';
+import { delay, put, take, takeEvery, takeLatest } from 'redux-saga/effects';
+import { increment, incrementSaga, incrementSagaSuccess } from './counterSlice';
 
-export function* log(action: PayloadAction) {
-  console.log('Log:', action);
+// export function* log(action: PayloadAction) {
+//   console.log('Log:', action);
+// }
+export function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log('Waiting 1 seconds...');
+  yield delay(1000);
+  console.log('Waiting done, dispatch action...');
+  yield put(incrementSagaSuccess(action.payload));
 }
 
 export default function* counterSaga() {
   // yield all([counterSaga()]);
   console.log('Counter saga');
-  yield takeEvery(increment().type, log); //Effect của saga lắng nghe action increment
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga); //Effect của saga lắng nghe action increment
+  // yield takeLatest(incrementSaga.toString(), handleIncrementSaga);
 }
