1f953a640c08ca4f1af74c85e5d402d8bf65f8ae
ttt <thanhtungcis60@gmail.com>
2025-10-10 11:26:19 +0700

    Redux Saga: 15 - Add Material UI library to reactjs typescript

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,27 @@
+import { Box, Button, Paper, Typography } from '@mui/material';
 import * as React from 'react';
 
-export interface LoginPageProps {}
-
-export default function LoginPage(props: LoginPageProps) {
-  return <div>Login Page</div>;
+export default function LoginPage() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexFlow: 'row nowrap',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Paper elevation={1} sx={{ p: 3 }}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+        <Box mt={4}>
+          <Button variant="contained" color="primary" fullWidth>
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
