c77c686ff7365b6361771314dfc7cc64afbfc510
ttt <thanhtungcis60@gmail.com>
2025-11-28 16:17:31 +0700

    Redux Saga: 44 - Làm form validation đơn giản với Yup

--- src/features/students/components/StudentForm.tsx
+++ src/features/students/components/StudentForm.tsx
@@ -1,11 +1,12 @@
-import { useForm } from 'react-hook-form';
-import { Student } from '../../../models';
 import { Box, Button } from '@mui/material';
-import { InputField, RadioGroupField } from '../../../component/FormFields';
-import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import { useAppSelector } from '../../../app/hooks';
-import { selectCityOptions } from '../../city/citySlice';
+import { InputField, RadioGroupField } from '../../../component/FormFields';
 import { SelectField } from '../../../component/FormFields/SelectField';
+import { Student } from '../../../models';
+import { selectCityOptions } from '../../city/citySlice';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export interface StudentFormProps {
   initialValues?: Student;
@@ -14,8 +15,39 @@ export interface StudentFormProps {
 
 export default function StudentForm({ initialValues, onSubmit }: StudentFormProps) {
   const cityOptions = useAppSelector(selectCityOptions);
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required('Please enter name.')
+      .test('two-words', 'Please enter at least two words', (value) => {
+        if (!value) return true;
+
+        const parts = value?.split(' ') || [];
+        return parts.filter((x) => !!x).length >= 2;
+      }),
+    age: yup
+      .number()
+      .positive('Please enter a positive number.')
+      .integer('Please enter an integer.')
+      .min(18, 'Min is 18.')
+      .max(80, 'Max is 80.')
+      .required('Please enter age.')
+      .typeError('Please enter a valid number'),
+    mark: yup
+      .number()
+      .min(0, 'Min is 0')
+      .max(10, 'Max is 10')
+      .required('Please enter mark.')
+      .typeError('Please enter a valid number'),
+    gender: yup
+      .string()
+      .oneOf(['male', 'female'], 'Please select either male or female.')
+      .required('Please select gender.'),
+    city: yup.string().required('Please select city.'),
+  });
   const { control, handleSubmit, reset } = useForm<Student>({
     defaultValues: initialValues,
+    resolver: yupResolver(schema),
   });
   //   useEffect(() => {
   //     reset(initialValues); // 👈 Cập nhật form khi initialValue thay đổi ~ API trả về dữ liệu
@@ -26,7 +58,7 @@ export default function StudentForm({ initialValues, onSubmit }: StudentFormProp
   };
 
   return (
-    <Box>
+    <Box maxWidth={400}>
       <form onSubmit={handleSubmit(handleFormSubmit)}>
         <InputField name="name" control={control} label="Full Name" />
         <RadioGroupField
